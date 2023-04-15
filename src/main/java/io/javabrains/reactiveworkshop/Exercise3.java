fc8d0788d488a76185ff6ff78e5ab5488051b2fe
dr-mogambo <beepingtom@gmail.com>
2023-04-15 06:49:07 -0400

    added things to the Project_

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,17 +1,21 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumbersFlux()
+        List<Integer> numbers = ReactiveSources.intNumbersFlux().toStream().toList();
+        // this is a blocking operation, where we first get all elements within the
+        // stream and this is why this is called blocking.
 
+        System.out.println("list is" + numbers);
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
         // TODO: Write code here
-
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,18 +1,23 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumberMono()
+        ReactiveSources.intNumberMono().subscribe(e -> System.out.println(e));
 
         // Print the value from intNumberMono when it emits
         // TODO: Write code here
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+        Optional<Integer> numbeer = ReactiveSources.intNumberMono().blockOptional();
+
+        numbeer.orElse(2);
 
         System.out.println("Press a key to end");
         System.in.read();
